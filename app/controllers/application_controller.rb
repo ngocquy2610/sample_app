35b249f58edbfde4571e3930dbe9055d7c59f6e1
quynn <nnquy2610@gmail.com>
2026-02-11 09:40:15 +0700

    Add a Static Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
+  def hello
+    render html: "Hello, World!"
+  end
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 end
