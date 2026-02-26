b537a7ae1de9807c569786e1503f7aa3c894877c
quynn <nnquy2610@gmail.com>
2026-02-26 16:16:27 +0700

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    # Handle a successful save.
-    flash[:success] = "Welcome to the Sample App!"
-    redirect_to @user
+      # Handle a successful save.
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       # Not the final implementation!
       render "new", status: :unprocessable_entity
